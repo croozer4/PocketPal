74f6a8d06c502b0c42cb1d7713c9e95652e6a4d3
croozer4 <konradkul12@gmail.com>
2023-10-09 15:24:46 +0200

    Basic Chart

--- pocketpal_app/src/App.tsx
+++ pocketpal_app/src/App.tsx
@@ -1,6 +1,41 @@
 import './App.css'
 import NavbarComponent from './components/NavbarComponent'
 import AuthComponent from './components/AuthenticationComponent'
+import BasicPieChart from './components/BasicPieChart'
+
+const data = [
+  {
+    "id": "lisp",
+    "label": "lisp",
+    "value": 114,
+    "color": "hsl(116, 70%, 50%)"
+  },
+  {
+    "id": "php",
+    "label": "php",
+    "value": 280,
+    "color": "hsl(20, 70%, 50%)"
+  },
+  {
+    "id": "go",
+    "label": "go",
+    "value": 409,
+    "color": "hsl(222, 70%, 50%)"
+  },
+  {
+    "id": "haskell",
+    "label": "haskell",
+    "value": 200,
+    "color": "hsl(271, 70%, 50%)"
+  },
+  {
+    "id": "make",
+    "label": "make",
+    "value": 486,
+    "color": "hsl(278, 70%, 50%)"
+  }
+]
+
 
 function App() {
   return (
@@ -12,6 +47,9 @@ function App() {
           </p>
       </div>
       <AuthComponent/>
+
+      <BasicPieChart data={data}/>
+      siema
     </>
   )
 }
