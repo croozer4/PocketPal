49ad7b684174c637ac260ecdf46fd3b1c3b62527
iconlynx <106556210+iconlynx@users.noreply.github.com>
2023-10-09 14:10:14 +0200

    feat(project): base vscode

---
+++ pocketpal_application/src/components/LoginComponent.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+
+import Auth from "./AuthenticationComponent.tsx"
+import { auth } from "../config/firebase";
+
+
+function App() {
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                setLoggedIn(true);
+                console.log("User logged in");
+            } else {
+                setLoggedIn(false);
+                console.log("User logged out");
+            }
+        });
+    });
+
+    return (
+        <div className="App">
+            <Auth/>
+            {loggedIn ? (
+                <h4>Użytkownik zalogowany</h4>
+            ) : (
+                <h4>Użytkownik niezalogowany</h4>
+            )}
+            {loggedIn ? (
+                <button onClick={() => auth.signOut()}>Wyloguj</button>
+            ) : null}
+        </div>
+    );
+}
+
+export default App;
