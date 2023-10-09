85f7ac6abf468868fd057804a6305636d4f50990
iconlynx <106556210+iconlynx@users.noreply.github.com>
2023-10-09 15:23:13 +0200

    feat(project): base navbar

--- pocketpal_app/src/App.tsx
+++ pocketpal_app/src/App.tsx
@@ -1,17 +1,17 @@
 import './App.css'
-import NavbarComponent from './components/NavbarComponent'
+import CustomNavbar from './components/NavbarComponent'
 import AuthComponent from './components/AuthenticationComponent'
 
 function App() {
   return (
     <>
-      <NavbarComponent/>
+      <CustomNavbar/>
       <div className="App">
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
       </div>
-      <AuthComponent/>
+      {/* <AuthComponent/> */}
     </>
   )
 }
